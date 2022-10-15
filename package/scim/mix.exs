a7584aa4412449efa185d93511e46239a15469c9
Rico Moorman <rico.moorman@gmail.com>
2022-10-15 04:28:32 +0200

    Bump versions and update changelog

--- package/scim/mix.exs
+++ package/scim/mix.exs
@@ -4,7 +4,7 @@ defmodule SCIM.MixProject do
   def project() do
     [
       app: :scim,
-      version: "0.0.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
